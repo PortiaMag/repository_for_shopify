e5683d3892ad21437b487888360297cf9ec47e90
Portia Matha <portia@mag.cr>
2024-02-07 16:45:16 +0200

    Helo Elizatheth

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liduid',
+    './templates/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
